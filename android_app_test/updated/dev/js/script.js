d7813ba6e6942c2920e6a5f4c32b81ffeaba186b
ashfaqpro <ashfaq.ace@gmail.com>
2014-05-09 04:44:11 +0530

    Update script.js

--- android_app_test/updated/dev/js/script.js
+++ android_app_test/updated/dev/js/script.js
@@ -126,8 +126,6 @@ function modifications() {
     $(".selectpicker").selectpicker("refresh");
 }
 
-	alert('hello');
-
 $('.container-wrapper-start-up').on('click', function(){
   $(this).addClass('hide-this');
   $('.container-wrapper-search-app').addClass('show-this');
